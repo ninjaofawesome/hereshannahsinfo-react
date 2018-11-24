bed66c46309023516b739275a4ded6daa20b4adf
Hannah Nordgren <hey@hereshannahs.info>
2018-11-24 18:19:28 -0500

    [FEATURE] added test suite

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapper = shallow(<App />);
+  const title = <h1 className='App-title'>Welcome to React</h1>;
+  expect(wrapper.contains(title)).toEqual(true);
 });
