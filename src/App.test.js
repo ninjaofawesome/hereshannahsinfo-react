b41045157a4cb7db550afd848a78e7957fd503a5
Hannah Nordgren <hey@hereshannahs.info>
2018-11-25 16:34:57 -0500

    [MISC] added describe wrapper to app test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const wrapper = shallow(<App />);
-  const title = <h1 className='App-title'>Welcome to React</h1>;
-  expect(wrapper.contains(title)).toEqual(true);
+describe('App Page', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<App />);
+    const title = <h1 className='App-title'>Welcome to React</h1>;
+    expect(wrapper.contains(title)).toEqual(true);
+  });
 });
+
