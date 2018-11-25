ed55479cf789f0a81bbcddea613fe65ed1bf6ebe
Hannah Nordgren <hey@hereshannahs.info>
2018-11-25 17:08:24 -0500

    [MISC] added space

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,12 @@ import { shallow } from 'enzyme';
 import App from './App';
 
 describe('App Page', () => {
+
   it('renders without crashing', () => {
     const wrapper = shallow(<App />);
     const title = <h1 className='App-title'>Welcome to React</h1>;
     expect(wrapper.contains(title)).toEqual(true);
   });
+
 });
 
