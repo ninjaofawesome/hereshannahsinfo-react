42653e80509deb9d34575599c53ded12f0975dab
Hannah Nordgren <hey@hereshannahs.info>
2018-12-11 16:56:26 -0500

    [WIP] setting up style sheets

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import './App.css';
+import styles from './App.module.scss';
 
 const App = () => (
-  <div className='App'>
-    <h1 className='App-title'>Welcome to React</h1>
+  <div className={styles.App}>
+    <h1 className={styles.appTitle}>Welcome to React</h1>
   </div>
 );
 
