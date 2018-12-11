3ca6701189cb2eb8ea50b54c9da7f134e9c32910
Hannah Nordgren <hey@hereshannahs.info>
2018-12-11 15:33:57 -0500

    [WIP] linting rules and globals

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import Blank from './';
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1 className='App-title'>Welcome to React</h1>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className='App'>
+    <h1 className='App-title'>Welcome to React</h1>
+  </div>
+);
 
 export default App;
