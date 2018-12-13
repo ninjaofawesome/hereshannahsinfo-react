7b207a36c4bcc6385b7f04f131c8ed6470dbeffa
Hannah Nordgren <hey@hereshannahs.info>
2018-12-12 21:33:25 -0500

    [FEATURE/STYLE] added and updated core styles

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -4,6 +4,7 @@ import styles from './App.module.scss';
 const App = () => (
   <div className={styles.App}>
     <h1 className={styles.appTitle}>Welcome to React</h1>
+    <p>"Oooh baby, you are looking sickening tonight! I have got'sta get me some of those heels!" admires <a href="#">Princess Pop</a></p>
   </div>
 );
 
