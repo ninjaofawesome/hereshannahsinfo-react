39c388ee97e3b0d84e76ea3d031c0af13ac552bd
Hannah Nordgren <hey@hereshannahs.info>
2018-12-14 16:02:00 -0500

    [WIP] set up list component

---
+++ src/components/List/List.test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import List from './List';
+
+describe('List Component', () => {
+  it('should dynamically render list items from an external data source', () => {
+
+  });
+
+  it('should not render any list items if no data is provided', () => {
+
+  });
+});
