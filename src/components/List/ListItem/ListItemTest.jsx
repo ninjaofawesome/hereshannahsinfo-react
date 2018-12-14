59bd0f3a22a2e72cdcb9823c15287ec0af687e15
Hannah Nordgren <hey@hereshannahs.info>
2018-12-14 16:12:31 -0500

    [WIP/CLEANUP] removed extra App.jsx file, created list component directory/files

---
+++ src/components/List/ListItem/ListItemTest.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ListItem from './ListItem';
+
+describe('List Item Component', () => {
+  it('should be able to accept child components if child components are provided', () => {
+
+  });
+
+  it('should accept text as the default content if none is provided', () => {
+
+  });
+});
