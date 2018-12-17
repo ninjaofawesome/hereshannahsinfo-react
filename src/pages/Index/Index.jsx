427d311f5471c525fbafca48cd1f4902a4ded0f8
Hannah Nordgren <hey@hereshannahs.info>
2018-12-17 17:22:29 -0500

    [STYLE] added initial container style

--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import Container from '../../components/Container/Container';
 import Title from '../../components/Title/Title';
 import List from '../../components/List/List';
 import styles from './Index.module.scss';
 
 const App = () => (
-  <div className={styles.App}>
-    <Title copy='Hello World!' />
-    <p>"Oooh baby, you are looking sickening tonight! I have got'sta get me some of those heels!" admires <a href="#">Princess Pop</a></p>
-    <List />
+  <div className={styles.indexPage}>
+    <Container>
+      <Title copy='Hello World!' />
+      <p>"Oooh baby, you are looking sickening tonight! I have got'sta get me some of those heels!" admires <a href="#">Princess Pop</a></p>
+      <List />
+    </Container>
   </div>
 );
 
