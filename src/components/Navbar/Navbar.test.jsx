b1714c4662fce1878d2d1cda7e6f540321fe43a4
Hannah Nordgren <hey@hereshannahs.info>
2019-01-02 21:52:41 -0500

    [FEATURE] app and navbar tests

--- src/components/Navbar/Navbar.test.jsx
+++ src/components/Navbar/Navbar.test.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
 import Navbar from './Navbar';
 
 describe('Navbar', () => {
+  const navbar = shallow(
+    <MemoryRouter>
+      <Navbar />
+    </MemoryRouter>,
+  );
+
   it('should render a menu click option', () => {
-    const navbar = shallow(<Navbar />);
-    console.log('navbar', navbar.html());
-  });
-
-  it('should open the menu on button click', () => {
-
-  });
-
-  it('should close the menu when clicking on an item', () => {
-
-  });
-
-  it('should link the site title to the index page', () => {
-
+    expect(navbar.props().history.entries[0].pathname).toEqual('/');
   });
 });
