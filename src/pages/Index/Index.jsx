260ded243086abc3586ace200122a1017d3ab751
Hannah Nordgren <hey@hereshannahs.info>
2019-08-13 00:14:03 -0400

    [WIP] creating intro component

--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -4,6 +4,33 @@ import Title from '../../components/Title/Title';
 import List from '../../components/List/List';
 import styles from './Index.module.scss';
 
+const dateObj = () => {
+  const startDate = new Date('03/14/2015').getTime();
+  const today = new Date().getTime();
+  const numberOfDays = (today - startDate) / (1000 * 3600 * 24);
+  const totalTime = (numberOfDays / 365).toFixed(2);
+  return totalTime;
+};
+
+const data = [
+  {
+    id: 1,
+    copy: () => `As of today, I have been a developer for ${dateObj()} years.`,
+  },
+  {
+    id: 2,
+    copy: () => 'I have a degree in fine art and I was a licenced hairdresser in 2 states!',
+  },
+  {
+    id: 3,
+    copy: () => 'JavaScript is my primary language.',
+  },
+  {
+    id: 3,
+    copy: () => 'I knit voraciously.',
+  },
+];
+
 const Index = () => (
   <div className={styles.indexPage}>
     <Container>
@@ -17,7 +44,8 @@ const Index = () => (
           copy='I make things.'
           size='subtitle'
         />
-        <List />
+        <p> Here are some things that you should know about me:</p>
+        <List {...data} />
       </div>
     </Container>
   </div>
